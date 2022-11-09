a008f6c8774b9ad603dc4cb9a9d7e7ee2d5b8147
Andrei <vasilescuandrei24@gmail.com>
2022-11-09 21:26:55 +0200

    first_updates

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,14 +6,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import checker.CheckerConstants;
-import fileio.Input;
+import fileio.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Objects;
+import java.util.Random;
 
 /**
  * The entry point to this homework. It runs the checker that tests your implentation.
@@ -71,7 +74,43 @@ public final class Main {
 
         //TODO add here the entry point to your implementation
 
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        ActionsInput actionsInput;
+        CardInput cardInput;
+        Coordinates coordinates;
+        DecksInput decksInput;
+        GameInput gameInput;
+        Input input;
+        StartGameInput startGameInput;
+
+        DecksInput player1_all_decks, player2_all_decks;
+        startGameInput = inputData.getGames().get(0).getStartGame();
+
+        player1_all_decks = inputData.getPlayerOneDecks();
+        player2_all_decks = inputData.getPlayerTwoDecks();
+
+        int index_player1_deck = startGameInput.getPlayerOneDeckIdx();
+        int index_player2_deck = startGameInput.getPlayerTwoDeckIdx();
+
+        ArrayList<CardInput> player1_deck = player1_all_decks.getDecks().get(index_player1_deck);
+        ArrayList<CardInput> player2_deck = player2_all_decks.getDecks().get(index_player2_deck);
+
+        Collections.shuffle(player1_deck, new Random(startGameInput.getShuffleSeed()));
+        Collections.shuffle(player2_deck, new Random(startGameInput.getShuffleSeed()));
+
+        player1.getHandCards().add(player1_deck.get(0));
+        player2.getHandCards().add(player2_deck.get(0));
+        player1_deck.remove(0);
+        player2_deck.remove(0);
+
+        CardInput player1_hero = startGameInput.getPlayerOneHero();
+        CardInput player2_hero = startGameInput.getPlayerTwoHero();
+
+//        System.out.println(player1_hero + "\n" + player2_hero);
+
         ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
-        objectWriter.writeValue(new File(filePath2), output);
+        objectWriter.writeValue(new File(filePath2), inputData);
     }
 }
---
+++ src/main/Minion.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Minion {
+    private int mana;
+    private int health;
+    private int attackDamage;
+    private String description;
+    private String colors;
+    private String name;
+
+
+}
