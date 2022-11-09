f311bd3b52b9e482898f8562af0b267e05ac31ff
andreivasilescu24 <vasilescuandrei24@gmail.com>
2022-11-09 23:43:44 +0200

    ++action_interpretor and player class

---
+++ src/main/ActionInterpretor.java
@@ -0,0 +1,22 @@
+package main;
+
+import fileio.ActionsInput;
+
+public class ActionInterpretor {
+
+public void getPlayerDeck(ActionsInput action) {
+
+}
+
+public void getPlayerHero(ActionsInput action) {
+
+}
+
+public void getPlayerTurn(ActionsInput action) {
+
+}
+
+
+
+
+}
---
+++ src/main/Player.java
@@ -0,0 +1,13 @@
+package main;
+
+import fileio.CardInput;
+
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<CardInput> handCards = new ArrayList<>();
+
+    public ArrayList<CardInput> getHandCards() {
+        return handCards;
+    }
+}
